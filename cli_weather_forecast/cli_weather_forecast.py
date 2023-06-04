1f02c60c8ce8438f7c1de4191d0d0d778f20f3cb
marjiasdk <marjiasdk@gmail.com>
2023-06-04 23:23:14 +0100

    project #1

---
+++ cli_weather_forecast/cli_weather_forecast.py
@@ -0,0 +1,7 @@
+"""
+
+This is a simple CLI weather forecast application that uses the OpenWeatherMap API to retrieve weather data for a given city.
+The user can input the following: city name, country code, and units of measurement.
+The application will return the following: city name, country code, current temperature, minimum temperature, maximum temperature, and weather description.
+
+"""
